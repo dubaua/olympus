86f96599c1b57d5bc4363a57e7483eff1df2066d
dubaua <dubaua@gmail.com>
2018-05-25 10:12:36 +0500

    add vue-webpack

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #app
+    | {{ test }}
+</template>
+
+<script>
+/* eslint-disable no-console, no-plusplus */
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      test: 'test',
+    };
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import 'normalize.css';
+import '@/styles/main.scss';
+import Vue from 'vue';
+import '@/components/globals';
+import App from './App';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  components: { App },
+  template: '<App/>',
+});
