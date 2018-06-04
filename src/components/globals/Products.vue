e503cefac272ed095466d4cfa0eecc37d6f64fec
dubaua <dubaua@gmail.com>
2018-06-04 13:27:32 +0500

    hide unnecessary columns

--- src/components/globals/Products.vue
+++ src/components/globals/Products.vue
@@ -12,10 +12,10 @@
       th.table__right Цена, руб.
       th.table__center НДС
       th.table__left Направление
-      th.table__left Комментарий
-      th.table__right Цена за предыдущий период
-      th.table__right Разница
-      th.table__left Старый код
+      //- th.table__left Комментарий
+      //- th.table__right Цена за предыдущий период
+      //- th.table__right Разница
+      //- th.table__left Старый код
       th.table__center Действие
     template(v-for="item in database")
       tr
@@ -25,10 +25,10 @@
         td.table__right {{ item.price }}
         td.table__center {{ item.vat }}
         td.table__left {{ item.section }}
-        td.table__left {{ item.comment }}
-        td.table__right {{ item.price_prev }}
-        td.table__right -
-        td.table__left {{ item.code_prev }}
+        //- td.table__left {{ item.comment }}
+        //- td.table__right {{ item.price_prev }}
+        //- td.table__right -
+        //- td.table__left {{ item.code_prev }}
         td.table__center
           template(v-if="alreadyExist(item.id)") Добавлено
           button(@click="add(item)", v-else) Добавить
