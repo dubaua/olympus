60af7d8abc12983dda3446b02894b96d4fd41310
dubaua <dubaua@gmail.com>
2018-06-04 21:59:49 +0500

    move database style to products component

--- src/components/globals/Products.vue
+++ src/components/globals/Products.vue
@@ -1,10 +1,33 @@
 <template lang="pug">
-.database(v-if="isActive")
-  .database__filters
+.products(v-if="isActive")
   table.table
     tr.table__head
-      th(colspan="11").tar
-        button(@click="$emit('toogle')") Закрыть
+      th(colspan="11")
+        .products__filters
+          //- .filter
+          //-   .filter__label Код
+          //-   .filter__control
+          //-     input(v-model="code")
+          //- .filter
+          //-   .filter__label Описание
+          //-   .filter__control
+          //-     input(v-model="description")
+          //- .filter
+          //-   .filter__label Направление
+          //-   .filter__control
+          //-     select(v-model="currentSection")
+          //-       option(value="") Все
+          //-       option(v-for="section in sections", value="section") {{ section }}
+          //- .filter
+          //-   .filter__label НДС
+          //-   .filter__control
+          //-     select(v-model="currentVAT")
+          //-       option(value="") Все
+          //-       option(:value="0") 0
+          //-       option(:value="10") 10
+          //-       option(:value="18") 18
+          .products__close
+            button(@click="$emit('toogle')") Закрыть
     tr.table__head
       th.tac Код
       th.tal Описание как в РУ
@@ -17,21 +40,21 @@
       //- th.tar Разница
       //- th.tal Старый код
       th.tac Действие
-    template(v-for="item in database")
+    template(v-for="position in products")
       tr
-        td.tac {{ item.id }}
-        td.tal {{ item.description_rc }}
-        td.tal {{ item.description }}
-        td.tar {{ item.price }}
-        td.tac {{ item.vat }}
-        td.tal {{ item.section }}
-        //- td.tal {{ item.comment }}
-        //- td.tar {{ item.price_prev }}
+        td.tac {{ position.id }}
+        td.tal {{ position.description_rc }}
+        td.tal {{ position.description }}
+        td.tar {{ position.price }}
+        td.tac {{ position.vat }}
+        td.tal {{ position.section }}
+        //- td.tal {{ position.comment }}
+        //- td.tar {{ position.price_prev }}
         //- td.tar -
-        //- td.tal {{ item.code_prev }}
+        //- td.tal {{ position.code_prev }}
         td.tac
-          template(v-if="alreadyExist(item.id)") Добавлено
-          button(@click="add(item)", v-else) Добавить
+          template(v-if="alreadyExist(position.id)") Добавлено
+          button(@click="add(position)", v-else) Добавить
 </template>
 
 <script>
@@ -39,8 +62,27 @@ export default {
   name: 'Products',
   props: {
     isActive: Boolean,
-    database: Object,
+    products: Object,
     current: Object,
+  },
+  data() {
+    return {
+      code: '',
+      description: '',
+      currentSection: '',
+      currentVAT: '',
+      sections: [
+        'MSE_CDS',
+        'MSE_ENG',
+        'MSE_ENT',
+        'MSE_ET',
+        'MSE_G&R',
+        'MSE_LAP IMG',
+        'MSE_SI',
+        'MSE_Uro Hys',
+        'MSE_Uro hys',
+      ],
+    };
   },
   methods: {
     add(item) {
@@ -56,4 +98,29 @@ export default {
 </script>
 
 <style lang="scss">
+.products {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-x: auto;
+  overflow-y: scroll;
+  background: white;
+
+  &__filters {
+    display: flex;
+  }
+  &__close {
+    margin-left: auto;
+  }
+}
+.filter {
+  display: flex;
+  align-items: center;
+  margin-right: 1em;
+  &__label {
+    margin-right: 0.4em;
+  }
+}
 </style>
