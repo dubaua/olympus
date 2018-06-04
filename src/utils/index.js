3e136c688716e24afb650ddcfdc0e28283c32903
dubaua <dubaua@gmail.com>
2018-06-04 16:41:11 +0500

    extract utility fuctions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,6 @@
+import accounting from 'accounting';
+
 /* eslint-disable import/prefer-default-export */
 export const numberWithSpaces = number => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+export const formatPrice = price => accounting.formatNumber(price, 2, ' ').replace('.', ',');
